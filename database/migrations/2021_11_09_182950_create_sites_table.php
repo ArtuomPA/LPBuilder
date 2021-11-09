56ef7dce7b38188256fda2abcf0d4800d45d8dca
ArtuomPA <gdambcz@mail.ru>
2021-11-09 19:16:20 +0000

    add sites models and migrations

---
+++ database/migrations/2021_11_09_182950_create_sites_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sites', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('color_scheme');
+            $table->string('background_img');
+            $table->string('favicon');
+            $table->string('logo');
+            $table->string('template');
+            $table->integer('user_id')->unsigned()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sites');
+    }
+}
